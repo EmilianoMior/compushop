779603717ee21a9b81a10695759de5ba3ca9183a
EmilianoMior <80928017+EmilianoMior@users.noreply.github.com>
2022-01-18 23:46:46 -0300

    Update App.js
    Entrega 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+
 import './App.css';
 import Header from './Components/Header';
+import ListaDeProductos from './Components/ListaDeProductos';
 
 function App() {
   return (
-
-    <Header
-      nombre="COMPUSHOP"
-    />
+    <>
+      <Header
+        nombre="COMPUSHOP"
+      />
+      <ListaDeProductos />
+    </>
   );
 }
 
 export default App;
+
